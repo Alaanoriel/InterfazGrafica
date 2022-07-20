6321e7d1e00fb86ae72d0e5e33bf50e983db0dfb
Alan Ibañez <Alaanoriel@gmail.com>
2022-07-19 23:41:14 -0300

    Baja producto

--- InterfazGrafica/FormularioBajaTotalEmpleado.h
+++ InterfazGrafica/FormularioBajaTotalEmpleado.h
@@ -67,7 +67,7 @@ namespace InterfazGrafica {
 			this->Txt_Dni->Font = (gcnew System::Drawing::Font(L"Century Gothic", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->Txt_Dni->ForeColor = System::Drawing::SystemColors::ButtonFace;
-			this->Txt_Dni->Location = System::Drawing::Point(12, 56);
+			this->Txt_Dni->Location = System::Drawing::Point(12, 36);
 			this->Txt_Dni->Name = L"Txt_Dni";
 			this->Txt_Dni->Size = System::Drawing::Size(141, 23);
 			this->Txt_Dni->TabIndex = 0;
@@ -75,23 +75,24 @@ namespace InterfazGrafica {
 			// 
 			// TxtBox_BajaTotal
 			// 
-			this->TxtBox_BajaTotal->Location = System::Drawing::Point(166, 61);
+			this->TxtBox_BajaTotal->Location = System::Drawing::Point(180, 36);
 			this->TxtBox_BajaTotal->Name = L"TxtBox_BajaTotal";
 			this->TxtBox_BajaTotal->Size = System::Drawing::Size(237, 20);
 			this->TxtBox_BajaTotal->TabIndex = 1;
 			// 
 			// Bton_Baja
 			// 
-			this->Bton_Baja->Location = System::Drawing::Point(166, 100);
+			this->Bton_Baja->Location = System::Drawing::Point(180, 62);
 			this->Bton_Baja->Name = L"Bton_Baja";
 			this->Bton_Baja->Size = System::Drawing::Size(75, 23);
 			this->Bton_Baja->TabIndex = 2;
 			this->Bton_Baja->Text = L"Dar de baja";
 			this->Bton_Baja->UseVisualStyleBackColor = true;
+			this->Bton_Baja->Click += gcnew System::EventHandler(this, &FormularioBajaTotalEmpleado::Bton_Baja_Click);
 			// 
 			// Bton_Salir
 			// 
-			this->Bton_Salir->Location = System::Drawing::Point(247, 100);
+			this->Bton_Salir->Location = System::Drawing::Point(261, 62);
 			this->Bton_Salir->Name = L"Bton_Salir";
 			this->Bton_Salir->Size = System::Drawing::Size(75, 23);
 			this->Bton_Salir->TabIndex = 3;
@@ -101,7 +102,7 @@ namespace InterfazGrafica {
 			// 
 			// Bton_Limpiar
 			// 
-			this->Bton_Limpiar->Location = System::Drawing::Point(328, 100);
+			this->Bton_Limpiar->Location = System::Drawing::Point(342, 62);
 			this->Bton_Limpiar->Name = L"Bton_Limpiar";
 			this->Bton_Limpiar->Size = System::Drawing::Size(75, 23);
 			this->Bton_Limpiar->TabIndex = 4;
@@ -114,7 +115,7 @@ namespace InterfazGrafica {
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->ClientSize = System::Drawing::Size(556, 163);
+			this->ClientSize = System::Drawing::Size(556, 128);
 			this->Controls->Add(this->Bton_Limpiar);
 			this->Controls->Add(this->Bton_Salir);
 			this->Controls->Add(this->Bton_Baja);
@@ -139,6 +140,10 @@ namespace InterfazGrafica {
 private: System::Void Bton_Salir_Click(System::Object^ sender, System::EventArgs^ e) {
 	
 	this->Close();
+
+}
+private: System::Void Bton_Baja_Click(System::Object^ sender, System::EventArgs^ e) {
+
 
 }
 };
--- InterfazGrafica/FormularioBajaTotalProducto.h
+++ InterfazGrafica/FormularioBajaTotalProducto.h
@@ -34,6 +34,12 @@ namespace InterfazGrafica {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ Txt_Codigo;
+	private: System::Windows::Forms::TextBox^ TxtBox_BajaTotalProd;
+	private: System::Windows::Forms::Button^ Bton_Baja;
+	private: System::Windows::Forms::Button^ Bton_Salir;
+	private: System::Windows::Forms::Button^ Bton_Limpiar;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,20 +54,90 @@ namespace InterfazGrafica {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->Txt_Codigo = (gcnew System::Windows::Forms::Label());
+			this->TxtBox_BajaTotalProd = (gcnew System::Windows::Forms::TextBox());
+			this->Bton_Baja = (gcnew System::Windows::Forms::Button());
+			this->Bton_Salir = (gcnew System::Windows::Forms::Button());
+			this->Bton_Limpiar = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// Txt_Codigo
+			// 
+			this->Txt_Codigo->AutoSize = true;
+			this->Txt_Codigo->Font = (gcnew System::Drawing::Font(L"Century Gothic", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Txt_Codigo->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->Txt_Codigo->Location = System::Drawing::Point(21, 38);
+			this->Txt_Codigo->Name = L"Txt_Codigo";
+			this->Txt_Codigo->Size = System::Drawing::Size(176, 23);
+			this->Txt_Codigo->TabIndex = 1;
+			this->Txt_Codigo->Text = L"Ingrese el codigo ";
+			// 
+			// TxtBox_BajaTotalProd
+			// 
+			this->TxtBox_BajaTotalProd->Location = System::Drawing::Point(203, 38);
+			this->TxtBox_BajaTotalProd->Name = L"TxtBox_BajaTotalProd";
+			this->TxtBox_BajaTotalProd->Size = System::Drawing::Size(237, 20);
+			this->TxtBox_BajaTotalProd->TabIndex = 2;
+			// 
+			// Bton_Baja
+			// 
+			this->Bton_Baja->Location = System::Drawing::Point(203, 67);
+			this->Bton_Baja->Name = L"Bton_Baja";
+			this->Bton_Baja->Size = System::Drawing::Size(75, 23);
+			this->Bton_Baja->TabIndex = 3;
+			this->Bton_Baja->Text = L"Dar de baja";
+			this->Bton_Baja->UseVisualStyleBackColor = true;
+			// 
+			// Bton_Salir
+			// 
+			this->Bton_Salir->Location = System::Drawing::Point(284, 67);
+			this->Bton_Salir->Name = L"Bton_Salir";
+			this->Bton_Salir->Size = System::Drawing::Size(75, 23);
+			this->Bton_Salir->TabIndex = 4;
+			this->Bton_Salir->Text = L"Salir";
+			this->Bton_Salir->UseVisualStyleBackColor = true;
+			this->Bton_Salir->Click += gcnew System::EventHandler(this, &FormularioBajaTotalProducto::Bton_Salir_Click);
+			// 
+			// Bton_Limpiar
+			// 
+			this->Bton_Limpiar->Location = System::Drawing::Point(365, 67);
+			this->Bton_Limpiar->Name = L"Bton_Limpiar";
+			this->Bton_Limpiar->Size = System::Drawing::Size(75, 23);
+			this->Bton_Limpiar->TabIndex = 5;
+			this->Bton_Limpiar->Text = L"Limpiar";
+			this->Bton_Limpiar->UseVisualStyleBackColor = true;
+			this->Bton_Limpiar->Click += gcnew System::EventHandler(this, &FormularioBajaTotalProducto::Bton_Limpiar_Click);
 			// 
 			// FormularioBajaTotalProducto
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->ClientSize = System::Drawing::Size(661, 420);
-			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->ClientSize = System::Drawing::Size(556, 128);
+			this->Controls->Add(this->Bton_Limpiar);
+			this->Controls->Add(this->Bton_Salir);
+			this->Controls->Add(this->Bton_Baja);
+			this->Controls->Add(this->TxtBox_BajaTotalProd);
+			this->Controls->Add(this->Txt_Codigo);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
 			this->Name = L"FormularioBajaTotalProducto";
-			this->Text = L"FormularioBajaTotalProducto";
+			this->Text = L"Baja producto";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void Bton_Salir_Click(System::Object^ sender, System::EventArgs^ e) {
+
+		this->Close();
+	}
+private: System::Void Bton_Limpiar_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	TxtBox_BajaTotalProd->Clear();
+}
+
+
+
+};
 }
